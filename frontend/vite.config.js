7b239a9b4f84b62bd443a86bef8961b3ea6298b7
dukim <dukim@dacon.kr>
2026-01-07 16:21:29 +0900

    build: setup base path and production api url for nginx reverse proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/conference/',
   server: {
     port: 9087, // Dev server port
   },
